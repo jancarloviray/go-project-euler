6b645ef386aef22abf7b58a6e9be27e1183f8a67
Jan Carlo Viray <jc.viray@bluecava.com>
2014-08-31 22:52:19 -0700

    getting biggest prime number solution

--- problem3.go
+++ problem3.go
@@ -6,8 +6,23 @@ What is the largest prime factor of the number 600851475143 ?
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main(){
-	fmt.Println("test")
+	// fmt.Println(getBiggestPrime(13195))
+	fmt.Println(getBiggestPrime(600851475143))
+}
+
+func getBiggestPrime(num int) int {
+	max := math.Sqrt(float64(num))
+	for i := int(max) ; i >= 2 ; i-- {
+		fmt.Println(i)
+		if num % i == 0 && getBiggestPrime(i) == 1 {
+			return i
+		}
+	}
+	return 1
 }
