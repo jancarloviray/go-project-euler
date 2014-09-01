8ca2b0f0aa7848be65c888640a8efc8aa146112b
Jan Carlo Viray <jc.viray@bluecava.com>
2014-08-31 21:46:16 -0700

    added conditionals using closure
    over-engineered, but trying to learn closures

--- problem2.go
+++ problem2.go
@@ -15,13 +15,33 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println(fib(10))
+	conditionOptions := condition(ConditionOptions{MaxResult:4000000})
+	fmt.Println(fib(conditionOptions))
 }
 
-func fib(terms int) []int {
+type ConditionOptions struct {
+	MaxResult int
+	MaxTerm int
+}
+
+func fib(condition func(int, int) bool) []int {
 	result := []int{1, 2}
-	for i := 2; i < terms; i++ {
-		result = append(result, result[i - 1] + result[i - 2])
+	for i := 2 ; ; i++ {
+		currentItem := result[i - 1] + result[i - 2]
+		if condition(currentItem, i) == false { break }
+		result = append(result, currentItem)
 	}
 	return result
+}
+
+func condition(options ConditionOptions) func(int, int) bool {
+	return func(currentItem int, currentTerm int) bool {
+		if options.MaxTerm == 0 && options.MaxResult != 0 {
+			return !(currentItem >= options.MaxResult)
+		}
+		if options.MaxResult == 0 && options.MaxTerm != 0 {
+			return !(currentTerm >= options.MaxTerm)
+		}
+		return true
+	}
 }
